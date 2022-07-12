f596cbe39b7b23ac3f32489c579bf7aa2297fba1
Morgan Spencer <morgan@spencercreative.co>
2022-07-11 22:00:52 -0400

    Add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head'
+
 import Footer from './Footer'
 import Header from './Header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div id="startersites-app">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Header />
       <main>
         <div className="inner">{children}</div>
